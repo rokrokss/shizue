058133e16e78ef237a694831badea64c202bf6c2
rokrokss <q0115643@gmail.com>
2025-05-21 04:52:33 +0400

    remove hot reload error log

--- src/hot-reload.ts
+++ src/hot-reload.ts
@@ -1,6 +1,10 @@
 if (import.meta.hot) {
   import.meta.hot.accept();
   import.meta.hot.dispose(() => {
-    chrome.runtime.reload();
+    try {
+      chrome.runtime.reload();
+    } catch (e) {
+      console.info('[HMR] context invalidated, ignored.');
+    }
   });
 }
