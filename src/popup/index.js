e0c40bca4bab43955e34446983d836b47a0059dc
rokrokss <q0115643@gmail.com>
2025-05-21 04:24:49 +0400

    cleanup

--- src/popup/index.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import ReactDOM from 'react-dom/client';
-import App from './App';
-const root = document.getElementById('root');
-ReactDOM.createRoot(root).render(_jsx(App, {}));
