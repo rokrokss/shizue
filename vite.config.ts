cea80d0845ed10c0d98b87a61442ece99639f73c
rokrokss <q0115643@gmail.com>
2025-05-21 04:29:07 +0400

    remove artifact zip

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
 import manifestConfig from './manifest.config';
-import zipPack from 'vite-plugin-zip-pack';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest: manifestConfig }),
-    zipPack({
-      inDir: 'dist',
-      outDir: 'artifacts',
-      outFileName: 'chrome.zip',
-    }),
-  ],
+  plugins: [react(), crx({ manifest: manifestConfig })],
   server: {
     port: 5173,
     hmr: {
