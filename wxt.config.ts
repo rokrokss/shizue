3962ac1e08c5a3de727bd057a8c15ef28766bee7
rokrokss <q0115643@gmail.com>
2025-05-22 02:14:32 +0400

    migrate to wxt

---
+++ wxt.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, type UserManifest } from 'wxt';
+import packageJson from './package.json';
+const { description } = packageJson;
+
+export default defineConfig({
+  modules: ['@wxt-dev/module-react'],
+  srcDir: 'src',
+  outDir: 'dist',
+  publicDir: 'src/public',
+  entrypointsDir: 'entrypoints',
+  manifestVersion: 3,
+  manifest: ({ browser, manifestVersion, mode, command }) => {
+    const manifest: UserManifest = {
+      name: 'Animal Crossing GPT',
+      description: description,
+      action: {
+        default_title: 'Animal Crossing GPT',
+      },
+    };
+    return manifest;
+  },
+  hooks: {
+    'build:manifestGenerated': (wxt, manifest) => {
+      if (wxt.config.mode === 'development') {
+        manifest.title += ' [DEV]';
+      }
+    },
+  },
+});
