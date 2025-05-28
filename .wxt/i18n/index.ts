7febab12c52359528606565ad2c47f2324d90bd0
rokrokss <q0115643@gmail.com>
2025-05-28 19:46:18 +0400

    ignore auto generated files

--- .wxt/i18n/index.ts
+++
@@ -1,6 +0,0 @@
-import { createI18n } from '@wxt-dev/i18n';
-import type { GeneratedI18nStructure } from './structure';
-
-export const i18n = createI18n<GeneratedI18nStructure>();
-
-export { type GeneratedI18nStructure }
