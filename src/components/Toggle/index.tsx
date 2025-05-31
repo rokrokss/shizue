35e80bd5f3340458c9e942a099d8cf84180c81f1
Giyeon Kim <dev@giyeon.kim>
2025-05-31 15:45:51 +0900

    chore: lint

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -92,91 +92,91 @@ const Toggle = () => {
     onDragStart={() => setIsDragging(true)}
     onDragEnd={() => setIsDragging(false)}
     className="sz:fixed sz:right-0 sz:bottom-[26px] sz:flex sz:flex-col sz:items-end sz:z-2147483647">
-    <div 
-      className="sz:flex sz:flex-col sz:items-end sz:z-2147483647" 
-      style={{ 
-        pointerEvents: isVisible ? 'auto' : 'none',
-      }}
-    >
-      <div
-        onMouseEnter={() => setIsHoveringMenu(true)}
-        onMouseLeave={() => setIsHoveringMenu(false)}
-        className={`
-          sz:flex sz:flex-col
-          sz:items-center
-          sz:shadow-md
-          sz:pb-[8px]
-          sz:pr-[8px]
-          sz:transition-all sz:duration-300
-          sz:z-2147483647
-          ${
-            isVisible
-              ? 'sz:opacity-100 sz:translate-x-0 sz:pointer-events-auto'
-              : 'sz:opacity-0 sz:translate-x-[8px] sz:pointer-events-none'
-          }
-      `}
-        style={{
-          transition: 'opacity 0.3s ease-in-out, translate 0.3s ease-in-out',
+      <div 
+        className="sz:flex sz:flex-col sz:items-end sz:z-2147483647" 
+        style={{ 
+          pointerEvents: isVisible ? 'auto' : 'none',
         }}
       >
-        <OverlayMenu>
-          {/* <OverlayMenuItem
-            icon={<SettingIcon className={`sz:w-[${menuIconSize}px] sz:h-[${menuIconSize}px]`} />}
-            tooltipMessage={tooltipMessages[0]}
-            onClick={() => handleTranslateSettingsOpenChange(!translateSettingsModalOpen)}
-            ref={translateSettingsPopoverTriggerRef}
-            popoverContent={
-              <TogglePopoverModal
-                triggerRef={translateSettingsPopoverTriggerRef}
-                onClose={() => handleTranslateSettingsOpenChange(false)}
-                content={
-                  <>
-                    <div className="sz:font-ycom sz:text-black sz:text-[14px] sz:mb-[2px] sz:text-center">
-                      {t('overlayMenu.translateSettings')}
-                    </div>
-                    <div className="sz:font-ycom sz:text-gray-700 sz:text-[12px] sz:text-center">
-                      ...
-                    </div>
-                  </>
-                }
-              />
+        <div
+          onMouseEnter={() => setIsHoveringMenu(true)}
+          onMouseLeave={() => setIsHoveringMenu(false)}
+          className={`
+            sz:flex sz:flex-col
+            sz:items-center
+            sz:shadow-md
+            sz:pb-[8px]
+            sz:pr-[8px]
+            sz:transition-all sz:duration-300
+            sz:z-2147483647
+            ${
+              isVisible
+                ? 'sz:opacity-100 sz:translate-x-0 sz:pointer-events-auto'
+                : 'sz:opacity-0 sz:translate-x-[8px] sz:pointer-events-none'
             }
-            isPopoverOpen={translateSettingsModalOpen}
-          /> */}
+        `}
+          style={{
+            transition: 'opacity 0.3s ease-in-out, translate 0.3s ease-in-out',
+          }}
+        >
+          <OverlayMenu>
+            {/* <OverlayMenuItem
+              icon={<SettingIcon className={`sz:w-[${menuIconSize}px] sz:h-[${menuIconSize}px]`} />}
+              tooltipMessage={tooltipMessages[0]}
+              onClick={() => handleTranslateSettingsOpenChange(!translateSettingsModalOpen)}
+              ref={translateSettingsPopoverTriggerRef}
+              popoverContent={
+                <TogglePopoverModal
+                  triggerRef={translateSettingsPopoverTriggerRef}
+                  onClose={() => handleTranslateSettingsOpenChange(false)}
+                  content={
+                    <>
+                      <div className="sz:font-ycom sz:text-black sz:text-[14px] sz:mb-[2px] sz:text-center">
+                        {t('overlayMenu.translateSettings')}
+                      </div>
+                      <div className="sz:font-ycom sz:text-gray-700 sz:text-[12px] sz:text-center">
+                        ...
+                      </div>
+                    </>
+                  }
+                />
+              }
+              isPopoverOpen={translateSettingsModalOpen}
+            /> */}
 
-          <OverlayMenuItem
-            icon={<TranslateIcon className={`sz:w-[${menuIconSize}px] sz:h-[${menuIconSize}px]`} />}
-            tooltipMessage={tooltipMessages[1]}
-            onClick={() => getPageTranslationService().toggle()}
-          />
+            <OverlayMenuItem
+              icon={<TranslateIcon className={`sz:w-[${menuIconSize}px] sz:h-[${menuIconSize}px]`} />}
+              tooltipMessage={tooltipMessages[1]}
+              onClick={() => getPageTranslationService().toggle()}
+            />
 
-          <OverlayMenuItem
-            icon={<BookIcon className={`sz:w-[${menuIconSize}px] sz:h-[${menuIconSize}px]`} />}
-            tooltipMessage={tooltipMessages[2]}
-            onClick={() => handleSummarizePage()}
-          />
-        </OverlayMenu>
+            <OverlayMenuItem
+              icon={<BookIcon className={`sz:w-[${menuIconSize}px] sz:h-[${menuIconSize}px]`} />}
+              tooltipMessage={tooltipMessages[2]}
+              onClick={() => handleSummarizePage()}
+            />
+          </OverlayMenu>
+        </div>
+        <div
+          onMouseEnter={() => setIsHoveringCharacter(true)}
+          onMouseLeave={() => setIsHoveringCharacter(false)}
+          className="sz:flex sz:items-center sz:justify-center sz:cursor-pointer sz:shadow-lg sz:shadow-cyan-400/20 sz:z-2147483647"
+          onClick={handleClick}
+          style={{
+            width: isVisible ? `${widthFull}px` : `${width}px`,
+            height: `${height}px`,
+            transition: 'width 0.3s ease-in-out',
+            background: 'linear-gradient( 135deg, #90F7EC 10%, #32CCBC 100%)',
+            borderTopLeftRadius: '9999px',
+            borderBottomLeftRadius: '9999px',
+            borderTopRightRadius: '0',
+            borderBottomRightRadius: '0',
+            pointerEvents: 'auto',
+          }}
+        >
+          <CharacterPickToggle index={characterIndex} />
+        </div>
       </div>
-      <div
-        onMouseEnter={() => setIsHoveringCharacter(true)}
-        onMouseLeave={() => setIsHoveringCharacter(false)}
-        className="sz:flex sz:items-center sz:justify-center sz:cursor-pointer sz:shadow-lg sz:shadow-cyan-400/20 sz:z-2147483647"
-        onClick={handleClick}
-        style={{
-          width: isVisible ? `${widthFull}px` : `${width}px`,
-          height: `${height}px`,
-          transition: 'width 0.3s ease-in-out',
-          background: 'linear-gradient( 135deg, #90F7EC 10%, #32CCBC 100%)',
-          borderTopLeftRadius: '9999px',
-          borderBottomLeftRadius: '9999px',
-          borderTopRightRadius: '0',
-          borderBottomRightRadius: '0',
-          pointerEvents: 'auto',
-        }}
-      >
-        <CharacterPickToggle index={characterIndex} />
-      </div>
-    </div>
     </motion.div>
   );
 };
